42a92fa5d5aecda680493a4500fc1be1d4592a63
Aashutosh24 <aashutosh@cleverground.co.in>
2025-09-07 15:26:22 +0530

    - Added Firebase configuration file - added app icon added new routes - added admin login page

--- src/components/Login/AdminLogin.js
+++ src/components/Login/AdminLogin.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const AdminLogin = () => {
     return (
         <View>
-            
+            <Text>Admin Login Screen</Text>
         </View>
     );
 }
