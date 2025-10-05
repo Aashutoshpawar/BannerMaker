dca05c804f23907fad1262867859d6c5fe8cbcb8
Aashutosh24 <aashutosh@cleverground.co.in>
2025-10-06 03:51:12 +0530

    feat: Implement user ID fetching and draft retrieval; enhance draft rendering and loading state

--- src/store/services/creationServices/CreationServices.js
+++ src/store/services/creationServices/CreationServices.js
@@ -1,12 +1,22 @@
 import Axios from "axios";
 import appConfig from "../../../constants/appConfig";
 
-export const createProject = async (projectData) => {
+export const createProject = async (Data) => {
     try {
-        const response = await Axios.post(`${appConfig.baseUrl}/creations`, projectData);
+        const response = await Axios.post(`${appConfig.baseUrl}/creations`, Data);
         return response.data;
     } catch (error) {
         console.error("Error creating project:", error);
+        // Optional: throw the error to be handled by caller");
+        throw error;
+    }
+};
+export const getProjectByUserID = async (Data) => {
+    try {
+        const response = await Axios.post(`${appConfig.baseUrl}/creations/user`, Data);
+        return response.data;
+    } catch (error) {
+        console.error("something went wrong:", error);
         // Optional: throw the error to be handled by caller
         throw error;
     }
